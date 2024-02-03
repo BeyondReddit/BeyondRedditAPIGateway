93770e8f87925ae589f372008c45ef84c6daa8c8
fuqing <fuqingreal@gmail.com>
2024-02-03 13:23:24 -0500

    Initial commit

---
+++ src/main/java/com/example/beyondredditapigateway/BeyondRedditApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.beyondredditapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondRedditApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeyondRedditApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/beyondredditapigateway/BeyondRedditApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beyondredditapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondRedditApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
